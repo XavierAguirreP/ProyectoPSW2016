fc6a0dc673f06a2778637d1867b3c07ee6fbd387
soccerdroid <mabegc@gmail.com>
2016-07-31 22:26:27 -0500

    Clase privilegio.php

---
+++ clases-php/privilegio.php
@@ -0,0 +1,28 @@
+<?php
+  /**
+   *
+   */
+  class Privilegio
+  {
+    private $privilegio;
+    private $codigo;
+    public function __construct($privilegio,$codigo)
+    {
+      $this->privilegio=$privilegio;
+      $this->codigo=$codigo;
+    }
+    public function getPrivilegio(){
+      return $this->privilegio;
+    }
+    public function getCodigo(){
+      return $this->codigo;
+    }
+    public function setPrivilegio($privilegio){
+      $this->privilegio=$privilegio;
+    }
+    public function setCodigo(){
+      $this->codigo=$codigo;
+    }
+  }
+
+?>
